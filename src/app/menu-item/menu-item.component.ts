81046dc028fdcb10a62986f8c03a23e2cf14f74c
suon vannputhika <vannputhika.suon22@kit.edu.kh>
2024-12-07 12:44:02 +0700

    cart works

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -1,23 +1,40 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogConfig, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { CartService } from '../cart/cart.service';
-import { AddToCartDialogComponent } from './add-to-cart-dialog.component';
+import { CartItem } from '../cart/cart-item.interface';
+
+export interface AddToCartDialogData {
+  title: string;
+  price: number;
+  imageSrc: string;
+}
 
 @Component({
   selector: 'app-menu-item',
-  templateUrl: './menu-item.component.html',
   standalone: true,
   imports: [
-    MatDialogModule
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule
   ],
+  templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.scss']
 })
 export class MenuItemComponent {
-  @Input() imageSrc!: string;
-  @Input() title!: string;
-  @Input() price!: number;
-  @Output() addToCartEvent = new EventEmitter<{ title: string; price: number; quantity: number }>();
+  @Input() imageSrc: string = '';
+  @Input() title: string = '';
+  @Input() price: number = 0;
+
+  @ViewChild('addToCartDialog', { static: true }) addToCartDialog!: TemplateRef<any>;
+
+  dialogQuantity: number = 1;
+  dialogSpecialInstructions: string = '';
 
   constructor(
       private dialog: MatDialog,
@@ -25,19 +42,29 @@ export class MenuItemComponent {
   ) {}
 
   addToCart() {
-    const dialogRef = this.dialog.open(AddToCartDialogComponent, {
-      width: '500px',
-      data: {
-        title: this.title,
-        price: this.price,
-        imageSrc: this.imageSrc
-      }
-    });
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.panelClass = 'centered-dialog';
+    dialogConfig.width = '600px';
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.cartService.addToCart(result);
-      }
-    });
+    const dialogRef = this.dialog.open(this.addToCartDialog, dialogConfig);
+
+    // Reset quantity and special instructions when dialog opens
+    this.dialogQuantity = 1;
+    this.dialogSpecialInstructions = '';
+  }
+
+  onAddToCart(dialogRef: MatDialogRef<any>): void {
+    const cartItem: CartItem = {
+      imageUrl: this.imageSrc,
+      title: this.title,
+      price: this.price,
+      quantity: this.dialogQuantity,
+      specialInstructions: this.dialogSpecialInstructions
+    };
+
+    this.cartService.addToCart(cartItem);
+    dialogRef.close();
   }
 }
